eb2058f8336ebd2b4d3a5f541c24b4d8ba34593e
Chris Cadden <chris.cadden@seeclickfix.com>
2020-10-22 12:13:21 -0400

    Create bands.band.songs nested routing

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -4,17 +4,69 @@ import { tracked } from '@glimmer/tracking';
 class Band {
   @tracked name;
 
-  constructor(name) {
+  constructor({ name, slug, songs }) {
     this.name = name;
+    this.slug = slug;
+    this.songs = songs;
+  }
+}
+
+class Song {
+  title = '';
+  rating = 0;
+  band = '';
+
+  constructor({ title, rating, band }) {
+    this.title = title;
+    this.rating = rating;
+    this.band = band;
   }
 }
 
 export default class BandsRoute extends Route {
   model() {
-    return [
-      new Band('Led Zeppelin'),
-      new Band('Pearl Jam'),
-      new Band('Foo Fighters')
-    ];
+    let blackDog = new Song({
+      title: 'Black Dog',
+      band: 'Led Zeppelin',
+      rating: 3
+    });
+
+    let yellowLedbetter = new Song({
+      title: 'Yellow Ledbetter',
+      band: 'Pearl Jam',
+      rating: 4
+    });
+
+    let daughter = new Song({
+      title: 'Daughter',
+      band: 'Pearl Jam',
+      rating: 5
+    });
+
+    let pretender = new Song({
+      title: 'The Pretender',
+      band: 'Foo Fighters',
+      rating: 2
+    });
+
+    let ledZeppelin = new Band({
+      name: 'Led Zeppelin',
+      slug: 'led-zeppelin',
+      songs: [blackDog]
+    });
+
+    let pearlJam = new Band({
+      name: 'Pearl Jam',
+      slug: 'pearl-jam',
+      songs: [yellowLedbetter, daughter]
+    });
+
+    let fooFighters = new Band({
+      name: 'Foo Fighters',
+      slug: 'foo-fighters',
+      songs: [pretender]
+    });
+    
+    return [ledZeppelin, pearlJam, fooFighters];
   }
 }
---
+++ app/routes/bands/band.js
@@ -0,0 +1,9 @@
+import Route from '@ember/routing/route';
+
+export default class BandsBandRoute extends Route {
+  model(params) {
+    let bands =
+      this.modelFor('bands');
+      return bands.find(band => band.slug === params.slug);
+  }
+}
