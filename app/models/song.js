9e8906aab5144c35672c35667ec8aacbca9e5c82
Chris Cadden <chris.cadden@seeclickfix.com>
2020-10-26 12:58:42 -0400

    Fix song rating after backend fetching

--- app/models/song.js
+++ app/models/song.js
@@ -1,4 +1,7 @@
+import { tracked } from '@glimmer/tracking';
+
 export default class Song {
+  @tracked rating;
   constructor({ id, title, rating, band }, relationships={}) {
     this.id = id
     this.title = title;
