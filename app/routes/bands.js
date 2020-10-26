29e697003d0d89f73de8abcd500f7155be6a9826
Chris Cadden <chris.cadden@seeclickfix.com>
2020-10-26 11:07:55 -0400

    Load bands from remote source

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,63 +1,19 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import Band from 'rarwe/models/band';
-import Song from 'rarwe/models/song';
+import fetch from 'fetch';
 
 export default class BandsRoute extends Route {
   @service catalog;
 
-  model() {
-    let blackDog = new Song({
-      title: 'Black Dog',
-      rating: 3
-    });
-
-    let yellowLedbetter = new Song({
-      title: 'Yellow Ledbetter',
-      rating: 4
-    });
-
-    let daughter = new Song({
-      title: 'Daughter',
-      rating: 5
-    });
-
-    let pretender = new Song({
-      title: 'The Pretender',
-      rating: 2
-    });
-
-    let ledZeppelin = new Band({
-      name: 'Led Zeppelin',
-      slug: 'led-zeppelin',
-      songs: [blackDog]
-    });
-
-    let pearlJam = new Band({
-      name: 'Pearl Jam',
-      slug: 'pearl-jam',
-      songs: [yellowLedbetter, daughter]
-    });
-
-    let fooFighters = new Band({
-      name: 'Foo Fighters',
-      slug: 'foo-fighters',
-      songs: [pretender]
-    });
-
-    blackDog.band = ledZeppelin;
-    yellowLedbetter.band = pearlJam;
-    daughter.band = pearlJam;
-    pretender.band = fooFighters;
-
-    this.catalog.add('song', blackDog);
-    this.catalog.add('song', yellowLedbetter);
-    this.catalog.add('song', daughter);
-    this.catalog.add('song', pretender);
-    
-    this.catalog.add('band', ledZeppelin);
-    this.catalog.add('band', pearlJam);
-    this.catalog.add('band', fooFighters);
+  async model() {
+    let response = await fetch('/bands');
+    let json = await response.json();
+    for(let item of json.data) {
+      let { id, attributes } = item;
+      let record = new Band({ id, ...attributes });
+      this.catalog.add('band', record);
+    }
 
     return this.catalog.bands;
   }
