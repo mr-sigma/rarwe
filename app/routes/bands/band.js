10691fd9545757e28ed915e4c12f9c0272abcd55
Chris Cadden <chris.cadden@seeclickfix.com>
2020-10-27 12:41:30 -0400

    BandNavigation component with active route highlighting

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -3,17 +3,8 @@ import { inject as service } from '@ember/service';
 
 export default class BandsBandRoute extends Route {
   @service catalog;
-  @service router;
 
   model(params) {
     return this.catalog.find('band', band => band.id === params.id);
-  }
-
-  redirect(band) {
-    if(band.description) {
-      this.router.transitionTo('bands.band.details');
-    } else {
-      this.router.transitionTo('bands.band.songs');
-    }
   }
 }
