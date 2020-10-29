b2c2264a9d94c4dd5d29c3891fde312f8cc60df5
Chris Cadden <chris.cadden@seeclickfix.com>
2020-10-29 08:59:52 -0400

    Add searching

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -7,8 +7,16 @@ export default class BandsBandSongsController extends Controller {
   @tracked showAddSong = true;
   @tracked title = '';
   @tracked sortBy = 'title';
+  @tracked searchTerm = '';
 
   @service catalog;
+
+  get matchingSongs() {
+    let searchTerm = this.searchTerm.toLowerCase();
+    return this.model.songs.filter(song => {
+      return song.title.toLowerCase().includes(searchTerm);
+    });
+  }
 
   get sortedSongs() {
     let sortBy = this.sortBy;
@@ -18,7 +26,7 @@ export default class BandsBandSongsController extends Controller {
       sortBy = this.sortBy.slice(1);
       isDescendingSort = true;
     }
-    return [...this.model.songs].sort((song1, song2) => {
+    return this.matchingSongs.sort((song1, song2) => {
       if(song1[sortBy] < song2[sortBy]) {
         return isDescendingSort ? 1 : -1;
       }
@@ -41,6 +49,11 @@ export default class BandsBandSongsController extends Controller {
   @action
   updateTitle(event) {
     this.title = event.target.value;
+  }
+
+  @action
+  updateSearchTerm(event) {
+    this.searchTerm = event.target.value;
   }
 
   @action
