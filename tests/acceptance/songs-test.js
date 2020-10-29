4a0f1cfe62a0463ab8a19d01f509054c48ee56e0
Chris Cadden <chris.cadden@seeclickfix.com>
2020-10-29 12:22:27 -0400

    Fix busted tests

--- tests/acceptance/songs-test.js
+++ tests/acceptance/songs-test.js
@@ -19,20 +19,20 @@ module('Acceptance | Songs', function(hooks) {
     await click('[data-test-rr=band-link]');
 
     assert.dom('[data-test-rr=song-list-item]:first-child').hasText('Elephants', 'The first song is the one that comes first in the alphabet');
-    assert.dom('[data-test-rr=song-list-item]:last-child').hasText('Spinning in Daffodils', 'The last song is the one that comes last in the alphabet');
+    assert.dom('[data-test-rr=song-list-item]:last-child').hasText('Spinning In Daffodils', 'The last song is the one that comes last in the alphabet');
 
     await click('[data-test-rr=sort-by-title-desc]');
-    assert.dom('[data-test-rr=song-list-item]:first-child').hasText('Spinning in Daffodils', 'The first song is the one that comes last in the alphabet');
+    assert.dom('[data-test-rr=song-list-item]:first-child').hasText('Spinning In Daffodils', 'The first song is the one that comes last in the alphabet');
     assert.dom('[data-test-rr=song-list-item]:last-child').hasText('Elephants', 'The last song is the one that comes first in the alphabet');
     assert.ok(currentURL().includes('s=-title'));
 
     await click('[data-test-rr=sort-by-rating-asc]');
     assert.dom('[data-test-rr=song-list-item]:first-child').hasText('Mind Eraser, No Chaser', 'The first song has the lowest rating');
-    assert.dom('[data-test-rr=song-list-item]:last-child').hasText('Spinning in Daffodils', 'The last song is the highest rated');
+    assert.dom('[data-test-rr=song-list-item]:last-child').hasText('Spinning In Daffodils', 'The last song is the highest rated');
     assert.ok(currentURL().includes('s=rating'));
 
     await click('[data-test-rr=sort-by-rating-desc]');
-    assert.dom('[data-test-rr=song-list-item]:first-child').hasText('Spinning in Daffodils', 'The first song has the highest rating');
+    assert.dom('[data-test-rr=song-list-item]:first-child').hasText('Spinning In Daffodils', 'The first song has the highest rating');
     assert.dom('[data-test-rr=song-list-item]:last-child').hasText('Mind Eraser, No Chaser', 'The last song is the lowest rated');
     assert.ok(currentURL().includes('s=-rating'));
   });
