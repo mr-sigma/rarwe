6f688a391d2ab89113519cdaac4009a3f2075ddf
Chris Cadden <chris.cadden@seeclickfix.com>
2020-10-29 11:09:48 -0400

    Remove waits simulating loadings

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,12 +1,10 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import wait from 'rarwe/utils/wait';
 
 export default class BandsRoute extends Route {
   @service catalog;
   
   async model() {
-    await wait(3000);
     return this.catalog.fetchAll('bands');
   }
 }
